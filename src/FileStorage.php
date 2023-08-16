5a12a9e47cb4bf465792ffcdd27171f1fc06af71
JonnyBo <evgeny.e.borisov@gmail.com>
2023-08-16 10:07:08 +0300

    вариант с файлами

--- src/FileStorage.php
+++ src/FileStorage.php
@@ -2,6 +2,8 @@
 
 namespace jonnybo\FileStorage;
 
+use Yii;
+
 class FileStorage implements FileStorageInterface
 {
     /**
@@ -17,7 +19,10 @@ class FileStorage implements FileStorageInterface
      * @return string
      */
     public function saveContent($object, $content) {
-
+        if (file_put_contents($object, $content) === false) {
+            return false;
+        }
+        return $object;
     }
 
     /**
@@ -25,7 +30,10 @@ class FileStorage implements FileStorageInterface
      * @return string
      */
     public function saveFile($object, $file) {
-
+        if (file_put_contents($object, $file) === false) {
+            return false;
+        }
+        return $object;
     }
 
     /**
@@ -41,7 +49,10 @@ class FileStorage implements FileStorageInterface
      * @return string
      */
     public function copy($object_to, $object_from) {
-
+        if (copy($object_from, $object_to) === false) {
+            return false;
+        }
+        return $object_to;
     }
 
     /**
@@ -49,7 +60,13 @@ class FileStorage implements FileStorageInterface
      * @return string
      */
     public function delete($objects) {
-
+        if (is_array($objects) && !empty($objects)) {
+            foreach ($objects as $object) {
+                @unlink($object);
+            }
+        } else {
+            @unlink($objects);
+        }
     }
 
     /**
@@ -57,7 +74,7 @@ class FileStorage implements FileStorageInterface
      * @return string
      */
     public function getName($object) {
-
+        return basename($object);
     }
 
     /**
@@ -65,6 +82,6 @@ class FileStorage implements FileStorageInterface
      * @return string
      */
     public function getUrl($object) {
-
+        return str_replace(Yii::$app->basePath . '/web/files', '', $object);
     }
 }
